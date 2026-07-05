1392358d7635ff99d7e42b34de6371edfadbe497
YumeMuzi <77779996+YumeMuzi@users.noreply.github.com>
2026-07-06 04:07:11 +0800

    删掉没用到的字体

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -10,12 +10,4 @@ export default defineUserConfig({
     lang: "zh-CN", title: "Yumu Docs", description: "Yumu Docs",
 
     theme,
-
-    // Enable it with pwa
-    // shouldPrefetch: false,
-    head: [["link", {
-        href: "/assets/font/AlibabaPuHuiTi-3-55-Regular.ttf", rel: "stylesheet",
-    },], ["link", {
-        href: "/assets/font/Torus-SemiBold.ttf", rel: "stylesheet",
-    },],],
 });
